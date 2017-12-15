4d9607eebfc1bd9e4c5c053dc9736b2161bd0d13
Gerald <gera2ld@163.com>
2017-12-15 23:48:38 +0800

    fix: connect to sqlite only once

--- wallpainter/pipelines.py
+++ wallpainter/pipelines.py
@@ -4,12 +4,6 @@ import json
 class SqlitePipeline:
     def __init__(self, filename):
         self.filename = filename
-
-    @classmethod
-    def from_crawler(cls, _crawler):
-        return cls('data/db.sqlite3')
-
-    def open_spider(self, spider):
         self.conn = sqlite3.connect(self.filename)
         self.cur = self.conn.cursor()
         self.cur.execute('''
@@ -22,10 +16,11 @@ extra TEXT)''')
 CREATE UNIQUE INDEX IF NOT EXISTS images_index ON images (source, url)
 ''')
 
-    def close_spider(self, spider):
-        self.conn.commit()
-        self.conn.close()
+    @classmethod
+    def from_crawler(cls, _crawler):
+        return cls('data/db.sqlite3')
 
     def process_item(self, item, spider):
         image = item['images'][0]
         self.cur.execute('INSERT OR IGNORE INTO images (source, url, filename, extra) VALUES (?, ?, ?, ?)', (spider.name, image['url'], image['path'], json.dumps(item['extra'])))
+        self.conn.commit()
