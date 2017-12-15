3ddf21b4b102d280cf4cd8069263449fe018cab3
Gerald <gera2ld@163.com>
2017-12-15 22:47:01 +0800

    fix: add start command

---
+++ wallpainter/__main__.py
@@ -0,0 +1,9 @@
+import scrapy
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+
+process = CrawlerProcess(get_project_settings())
+
+process.crawl('bing')
+process.crawl('wallpaperflare')
+process.start()
--- wallpainter/settings.py
+++ wallpainter/settings.py
@@ -16,7 +16,7 @@ NEWSPIDER_MODULE = 'wallpainter.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'wallpainter (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
