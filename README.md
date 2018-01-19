ba67e4cb1b2b803327b0a76c5afbb22f16a082ea
Gerald <gera2ld@163.com>
2018-01-19 16:35:51 +0800

    fix: kill backend on exit

--- README.md
+++ README.md
@@ -1,8 +1,28 @@
 wallpainter
----
+===
 
 Download images as desktop wallpapers.
 
+Development
+---
+
 ```sh
-$ python3 -m wallpainter
+# Prepare backend
+$ pip3 install -r requirements.txt
+
+# Prepare frontend
+$ cd app
+$ yarn
+
+# Start
+$ cd app
+$ yarn dev:core
+```
+
+Start
+---
+
+```sh
+$ cd app
+$ yarn start
 ```
--- app/rpc.js
+++ app/rpc.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const { spawn } = require('child_process');
 
-const child = spawn('python', [
+const child = spawn(process.env.PYTHON || 'python', [
   '-m',
   'wallpainter.rpc',
-]);
+], {
+  cwd: path.resolve('..'),
+});
 child.stdout.on('data', data => {
   process.stdout.write(String(data));
 });
@@ -13,4 +15,10 @@ child.stderr.on('data', data => {
 });
 child.on('close', code => {
   console.log('exit', code);
+});
+
+process.on('SIGINT', () => { process.exit(); });
+process.on('SIGTERM', () => { process.exit(); });
+process.on('exit', () => {
+  child.kill('SIGINT');
 });
