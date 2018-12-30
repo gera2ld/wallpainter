71e054d41c28348bf9800b7f62823372f52a77ce
Gerald <gera2ld@163.com>
2018-12-31 00:08:40 +0800

    chore: upgrade deps

--- app/scripts/webpack.conf.js
+++ app/scripts/webpack.conf.js
@@ -1,5 +1,14 @@
-const { html } = require('webpack-util/webpack');
-const config = require('./webpack.base.conf');
+const baseConfigProvider = require('webpack-util/config/webpack.base.conf');
+const webpackUtil = require('webpack-util/webpack');
+const { parseConfig } = require('webpack-util/util');
 const pages = require('./pages.conf');
 
-module.exports = async () => html({ pages })(await config);
+module.exports = async () => {
+  const config = await parseConfig(baseConfigProvider);
+  config.externals = {
+    ...config.externals,
+    electron: 'require("electron")',
+  };
+  config.devServer.port = 3000;
+  return webpackUtil.html({ pages })(config);
+};
